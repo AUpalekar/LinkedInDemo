6f3e27f8cb663af386f7229481d68004e6c2af74
Ashish Upalekar <Ashish Upalekar@LPCL-IKE908D4>
2017-07-25 18:10:01 +0530

    Add project files.

---
+++ DemoLinkedINLogin/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using DemoLinkedINLogin.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace DemoLinkedINLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult LinkedIN()
+        {
+            //Need to install below library
+            //Install-Package Restsharp
+            
+            return View();
+        }
+
+        public ActionResult LinkedINAuth(string code, string state)
+        {
+            try
+            {
+                //Get Accedd Token
+                 var client = new RestClient("https://www.linkedin.com/oauth/v2/accessToken");
+                var request = new RestRequest(Method.POST);
+                request.AddParameter("grant_type", "authorization_code");
+                request.AddParameter("code", code);
+                request.AddParameter("redirect_uri", "http://localhost:57633/Home/LinkedINAuth");
+
+                //request.AddParameter("client_id", "client id");
+                request.AddParameter("client_id", "7797u563ns17ca");
+
+                //request.AddParameter("client_secret", "client secret");
+                request.AddParameter("client_secret", "MfvyVy3PYGxlNqC9");
+                IRestResponse response = client.Execute(request);
+                var content = response.Content;
+
+                //Fetch AccessToken
+                JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+                LinkedINVM linkedINVM = jsonSerializer.Deserialize<LinkedINVM>(content);
+
+                //Get Profile Details
+                client = new RestClient("https://api.linkedin.com/v1/people/~?oauth2_access_token=" + linkedINVM.access_token + "&format=json");
+                request = new RestRequest(Method.GET);
+                response = client.Execute(request);
+                content = response.Content;
+
+                jsonSerializer = new JavaScriptSerializer();
+                LinkedINResVM linkedINResVM = jsonSerializer.Deserialize<LinkedINResVM>(content);
+
+                //Code for PDF generation
+
+                //GeneratePDF(linkedINResVM);
+
+                //PDF generation code ends
+                return View(linkedINResVM);
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void GeneratePDF(LinkedINResVM linkedINResVM)
+        {
+            using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream())
+            {
+                Document document = new Document(PageSize.A4, 10, 10, 10, 10);
+
+                PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+                string pdfName = linkedINResVM.headline;
+                //string path = Server.MapPath("../Resumes") +"//"+ pdfName + ".pdf";
+                //PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(path, FileMode.Create));
+
+
+                document.Open();
+
+                Chunk chunk = new Chunk("This is from chunk. ");
+                document.Add(chunk);
+
+                Phrase phrase = new Phrase("This is from Phrase.");
+                document.Add(phrase);
+
+                Paragraph para = new Paragraph("This is from paragraph.");
+                document.Add(para);
+
+                string text = linkedINResVM.firstName + "\r\n" + linkedINResVM.lastName + "\r\n";
+                Paragraph paragraph = new Paragraph();
+                paragraph.SpacingBefore = 10;
+                paragraph.SpacingAfter = 10;
+                paragraph.Alignment = Element.ALIGN_LEFT;
+                paragraph.Font = FontFactory.GetFont(FontFactory.HELVETICA, 12f, BaseColor.GREEN);
+                paragraph.Add(text);
+                document.Add(paragraph);
+
+                document.Close();
+                byte[] bytes = memoryStream.ToArray();
+                memoryStream.Close();
+                Response.Clear();
+                Response.ContentType = "application/pdf";
+
+
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + pdfName + ".pdf");
+                Response.ContentType = "application/pdf";
+                Response.Buffer = true;
+                Response.Cache.SetCacheability(System.Web.HttpCacheability.NoCache);
+                Response.BinaryWrite(bytes);
+
+
+
+                Response.End();
+                Response.Close();
+            }
+        }
+    }
+}
---
+++ DemoLinkedINLogin/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DemoLinkedINLogin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoLinkedINLogin")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ba5c1f7-c7b8-4a5f-93ec-bbc5f9653e30")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
